e9cbe40265ed292b5465a4e3e0add4d6d4be88f7
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2018-03-11 14:42:20 +0000

    Added in String Starter

---
+++ StringExercise/Assets/Scripts/DisplayCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterStats
+{
+    public string Name;
+    public int Health;
+    public int Strength;
+    public int Mana;
+}
+
+public class DisplayCharacter : MonoBehaviour {
+
+    //UI Text to hold the text
+    public Text UIText;
+
+    //The characters stats, this is what we are displaying
+    //to the user
+    private CharacterStats characterStats;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Initialise the chacter stats to default values
+        characterStats = new CharacterStats();
+        characterStats.Name = "Jo";
+        characterStats.Health = 80;
+        characterStats.Mana = 0;
+        characterStats.Strength = 10;
+
+        //Replace this with a string builder
+        UIText.text = "Name: " + characterStats.Name + "\nHealth: " +
+            characterStats.Health.ToString() + "\nStrength:  " +
+            characterStats.Strength.ToString() + "\nMana: " +
+            characterStats.Mana;
+    }
+}
